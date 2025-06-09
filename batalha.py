5dbeb7a672af1f67712efc1fdd2a1d11e9f04308
EricJuanDM <ericjuandm@gmail.com>
2025-06-09 00:31:21 -0300

    Consegui fazer essa porra
    Chupa gui

--- batalha.py
+++ batalha.py
@@ -24,19 +24,19 @@ def menuplayer():
 
 def atqPlayerLinha():
     while True:
-        playerLinha = int(input("\nEscolha uma linha entre 1 e 5: "))
+        playerLinha = int(input("\nescolhaColunaa uma linha entre 1 e 5: "))
         if 1 <= playerLinha <= 5:
             return playerLinha - 1
         else:
-            print("Linha inválida! Escolha entre 1 e 5.")
+            print("Linha inválida! escolhaColunaa entre 1 e 5.")
 
 def atqPlayerColuna():
     while True:
-        playerColuna = int(input("\nEscolha uma coluna entre 1 e 10: "))
+        playerColuna = int(input("\nescolhaColunaa uma coluna entre 1 e 10: "))
         if 1 <= playerColuna <= 10:
             return playerColuna - 1
         else:
-            print("Coluna inválida! Escolha entre 1 e 10.")
+            print("Coluna inválida! escolhaColunaa entre 1 e 10.")
 
 
 
@@ -100,38 +100,98 @@ compNav = 0
 
 
 #Define posições dos navios do computador
-while compNav < 5:
-    escolhaA = random.randint(0, 9)
-    escolhaB = random.randint(0, 9)
-    #impede que o computador escolha no mesmo lugar
-    if compmatriz5[escolhaA][escolhaB] != "1":
-        compNav +=1
-    compmatriz5[escolhaA][escolhaB] = "1"
-
+while True:
+    comparacao = 0
+    tamanhoNav =  5 - compNav
+    tamanhoCerto = 5 - compNav
+    escolhaLinha = random.randint(0, 9)
+    escolhaColuna = random.randint(0, 9)
+    direcao = random.randint(0,3) #0 = horizontal direita, 1 = horizontal esquerta, 2 = vertical baixo, 3 vertigal cima
+    
+    if escolhaColuna + tamanhoNav <= 10 and direcao == 0 and compmatriz5[escolhaLinha][escolhaColuna] == "-": 
+        for i in range(tamanhoNav):
+            if compmatriz5[escolhaLinha][escolhaColuna] == "-":
+                compmatriz5[escolhaLinha][escolhaColuna] = "1"
+                escolhaColuna = escolhaColuna + 1
+                tamanhoNav -= 1 
+                comparacao += 1
+            elif compmatriz5[escolhaLinha][escolhaColuna] == "1":
+                escolhaColuna = escolhaColuna - 1
+                compmatriz5[escolhaLinha][escolhaColuna] = "-"
+                tamanhoNav -= 1 
+                comparacao = 0
+        if comparacao == tamanhoCerto: 
+            compNav += 1
+    if escolhaColuna - tamanhoNav >= 0 and direcao == 1 and compmatriz5[escolhaLinha][escolhaColuna] == "-":
+        for i in range(tamanhoNav):
+            if compmatriz5[escolhaLinha][escolhaColuna] == "-":
+                compmatriz5[escolhaLinha][escolhaColuna] = "1"
+                escolhaColuna = escolhaColuna - 1
+                tamanhoNav -= 1 
+                comparacao += 1
+            elif compmatriz5[escolhaLinha][escolhaColuna] == "1":
+                escolhaColuna = escolhaColuna + 1
+                compmatriz5[escolhaLinha][escolhaColuna] = "-"
+                tamanhoNav -= 1
+                comparacao = 0 
+        if comparacao == tamanhoCerto: 
+            compNav += 1
+    if escolhaLinha + tamanhoNav <= 10 and direcao == 2 and compmatriz5[escolhaLinha][escolhaColuna] == "-": 
+        for i in range(tamanhoNav):
+            if compmatriz5[escolhaLinha][escolhaColuna] == "-":
+                compmatriz5[escolhaLinha][escolhaColuna] = "1"
+                escolhaLinha = escolhaLinha + 1
+                tamanhoNav -= 1 
+                comparacao += 1
+            elif compmatriz5[escolhaLinha][escolhaColuna] == "1":
+                escolhaLinha = escolhaLinha - 1
+                compmatriz5[escolhaLinha][escolhaColuna] = "-"
+                tamanhoNav -= 1 
+                comparacao = 0
+        if comparacao == tamanhoCerto: 
+            compNav += 1
+    if escolhaLinha - tamanhoNav >= 0 and direcao == 3 and compmatriz5[escolhaLinha][escolhaColuna] == "-":            
+        for i in range(tamanhoNav):
+            if compmatriz5[escolhaLinha][escolhaColuna] == "-":
+                compmatriz5[escolhaLinha][escolhaColuna] = "1"
+                escolhaLinha = escolhaLinha - 1
+                tamanhoNav -= 1 
+                comparacao += 1
+            elif compmatriz5[escolhaLinha][escolhaColuna] == "1":
+                escolhaLinha = escolhaLinha + 1
+                compmatriz5[escolhaLinha][escolhaColuna] = "-"
+                tamanhoNav -= 1 
+                comparacao = 0
+        if comparacao == tamanhoCerto: 
+            compNav += 1
+    #impede que o computador escolhaColunaa no mesmo lugar
+    if compNav == 5:
+        break
+        
 #Define posições dos navios do jogador
 i = 0
-while playerNav < 5:
+while playerNav < 10:
     i += 1
-    linha = int(input(f"Escolha a {i}° linha do seu navio: "))
+    linha = int(input(f"escolhaColunaa a {i}° linha do seu navio: "))
     while linha > 10:
-        print("Escolha uma linha menor ou igual a 10!")
-        linha = int(input(f"Escolha a {i}° linha do seu navio: "))
+        print("escolhaColunaa uma linha menor ou igual a 10!")
+        linha = int(input(f"escolhaColunaa a {i}° linha do seu navio: "))
 
-    coluna = int(input(f"Escolha a {i}° coluna do seu navio: "))
+    coluna = int(input(f"escolhaColunaa a {i}° coluna do seu navio: "))
     while coluna > 10:
-        print("Escolha uma coluna menor ou igual a 10!")
-        coluna = int(input(f"Escolha a {i}° coluna do seu navio: "))
+        print("escolhaColunaa uma coluna menor ou igual a 10!")
+        coluna = int(input(f"escolhaColunaa a {i}° coluna do seu navio: "))
 
     if linha <=10:
         linha -= 1
     if coluna <=10:
         coluna -= 1
 
-    # impede que o jogador escolha no mesmo lugar
+    # impede que o jogador escolhaColunaa no mesmo lugar
     if playermatriz5[linha][coluna] != "1":
         playerNav +=1
     else:
-        print("\nLinha e coluna já escolhida!\nEscolha outra!")
+        print("\nLinha e coluna já escolhaColunaida!\nescolhaColunaa outra!")
         i -= 1
     playermatriz5[linha][coluna] = "1"
 
@@ -149,7 +209,7 @@ while playerNav > 0 and compNav > 0:
     playerColuna = atqPlayerColuna()
 
     while tabuleiroComp[playerLinha][playerColuna] == "O" or tabuleiroComp[playerLinha][playerColuna] == "X":
-        print("\nLinha e Coluna já escolhidas!")
+        print("\nLinha e Coluna já escolhaColunaidas!")
         playerLinha = atqPlayerLinha()
         playerColuna = atqPlayerColuna()
 
